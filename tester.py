0af0ea79ae0be6869b5fe1eeb607995c7d6de2b7
yasyasyu <yasyu.hitoha@gmail.com>
2023-04-25 06:30:53 +0900

    update tester

--- tester.py
+++ tester.py
@@ -8,25 +8,74 @@ in_folder = pwd_folder + r"input/"
 out_folder = pwd_folder + r"output/"
 cmd_folder = pwd_folder + r"steiner-space-travel/"
 
+run_cmd = "n"
 arg_command = "./a.out"
 file_number = 0
 if len(args) > 1:
-	file_number = args[1]
-	
+	if args[1].isdecimal():
+		file_number = args[1]
+		if len(args) > 2:
+			if not args[2].isdecimal():
+				run_cmd = args[2]
+			else:
+				run_cmd = "p"
+		else:
+			run_cmd = "n"
+	else:
+		run_cmd = args[1]
+		if len(args) > 2:
+			if args[2].isdecimal():
+				file_number = args[2]
+			else:
+				run_cmd = "p"
+
 file = f"{str(file_number).zfill(4)}"
 in_file =  f"{in_folder}{file}.txt"
 out_file = f"{out_folder}{file}.txt"
+img_folder = "img/"
+img_file = f"{pwd_folder}{img_folder}{file}.png"
 
-cmd = f"{arg_command} < {in_file} > {out_file}"
-subprocess.run(
-	cmd,
-	shell=True,
-	cwd = pwd_folder
-)
+def run(cmd):
+	print(cmd)
+	subprocess.run(
+		cmd,
+		shell=True,
+		cwd = pwd_folder
+	)
+def normal():
+	cmd = f"{arg_command} < {in_file} > {out_file}"
+	run(cmd)
+	cmd = f"dotnet {cmd_folder}tester.dll judge -i {in_file} -o {out_file}"
+	run(cmd)
 
-cmd = f"dotnet {cmd_folder}tester.dll judge -i {in_file} -o {out_file}"
-subprocess.run(
-	cmd,
-	shell=True,
-	cwd = pwd_folder
-)
+def vis():
+	cmd = f"{arg_command} < {in_file} > {out_file}"
+	run(cmd)
+	cmd = f"dotnet {cmd_folder}tester.dll judge -i {in_file} -o {out_file} -v {img_file}"
+	run(cmd)
+
+def all():
+	parallel = 8
+	cmd = f"dotnet {cmd_folder}tester.dll judge-all -i {in_folder} -c {arg_command} -p {parallel}"
+	subprocess.run(
+		cmd,
+		shell=True,
+		cwd = pwd_folder
+	)
+
+
+def panic():
+	print("Panic!")
+	exit()
+
+if not run_cmd in "nvap":
+	panic()
+
+run_act = {
+	"n": normal,
+	"v": vis,
+	"a": all,
+	"p": panic
+}
+
+run_act[run_cmd]()
